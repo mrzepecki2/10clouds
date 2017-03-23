29495c83a0e3eb3348a9220ec46631d4f00f1966
mrzepecki2 <mateusz0345@gmail.com>
2017-03-23 10:08:40 +0100

    Dodanie

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl" ng-app="projekt">
+	<head>
+		<title>Strona testowa</title>
+		<meta name="viewport" content="initial-scale=1.0">
+		<meta charset="UTF-8">
+		<meta name="language" content="pl" />
+		<!--js-->
+    	<script src="js/jquery.js"></script>
+    	<script src="js/jquery-ui.min.js"></script>
+    	<script src="js/moment.js"></script>
+    	<script src="js/main.js"></script>
+    	
+        <!--Bootstrap-->
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" >
+        <script src="bootstrap/js/bootstrap.min.js"></script>   
+        <!-- css -->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+         <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+        <link rel="stylesheet" type="text/css" href="fonts/fonts.css" >
+
+        <meta name="Author" content="Mateusz Rzepecki" />
+	</head>
+	<body>
+		<header>
+			
+		</header>
+		<section class="backgrounds">
+			<img class="bg-1" src="img/background-1.png"/>
+			<img class="bg-2" src="img/background-2.png"/>
+		</section>
+		<main id="main-form">
+			<div class="container">
+					<div class="row">
+						<div class="form-right-header col-sm-offset-5 col-sm-5">
+							<h2>Your account</h2>
+						</div>
+					</div>
+					<div class="row form-outter">
+						<div class="form-left hidden-xs col-sm-offset-1 col-sm-3 col-md-offset-1 col-md-4 col-lg-offset-2 col-lg-3">
+							<div class="row">
+								<div class="form-left-inner">
+								</div>
+							</div>	
+						</div>
+						<div class="form-right  col-sm-7 col-md-6 col-lg-5">
+							<div class="form-right-inner">
+								<div class="p15">
+									Take it all with you. Switch between devices, and pick up.
+								</div>
+								<div class="form">
+									<form id="account-form" method="get" >
+										<label>Your name</label>
+										<input type="text" name="name" required/>
+
+										<label>Mobile</label>
+										<div class="row">
+											<div class="col-xs-5 col-sm-4 styled-select">
+												<select class="default" name="prefix">
+													<option>+48 (PL)</option>
+													<option>+48 (PL)</option>
+													<option>+48 (PL)</option>
+													<option>+48 (PL)</option>
+												</select>
+											</div>
+											<div class="col-xs-7 col-sm-8">
+												<input type="text" name="phone" onkeyup="this.value=this.value.replace(/[^0-9\-\s]/g,'');" required> 
+											</div>
+										</div>
+										<label>Gender</label>
+										<div class="row">
+											<div class="gender col-xs-12">
+												<fieldset id="group1" >
+													<label class="gender">
+														<i class="fa fa-2x fa-venus" aria-hidden="true"></i>
+														<input type="radio" name="gender" value="male" required/>
+													</label>
+													<label class="gender">
+														<i class="fa fa-2x fa-mars" aria-hidden="true"></i>
+														<input type="radio" name="gender" value="female" />
+													</label>
+												</fieldset>
+											</div>
+										</div>
+										<label>Date of Birth</label>
+										<div class="row">
+											<div class="col-xs-12 dates">
+												<select class="date-select date-1" name="date-day" required> 
+													<?php
+														for ($day=1; $day<=31; $day++) {
+													     	echo "<option>".$day ."</option>";
+												     	}
+													?>
+												</select>
+												<span class="dateaa"></span>
+												<select class="date-select date-2" name="date-month" required>
+													<?php
+														for ($m=1; $m<=12; $m++) {
+													     	$month = date("F", mktime(0,0,0,$m, 1, date("Y")));
+													     	echo "<option>".$month ."</option>";
+												     	}
+													?>
+												</select>
+
+												<span class="dateaa"></span>
+												<select class="date-select date-3" name="date-year" required>
+													<?php  
+														$current_year = intval(date("Y"));
+														for ($year = 1900; $year <= $current_year; $year++) { 
+															echo"<option>".$year."</option>";
+														}
+													?>
+												</select>
+											</div>
+											
+										</div>
+										<input id="submit-hidden" type="submit" value="Submit" hidden>
+									</form>
+								</div>
+								<div class="submit-form">
+									<div class="submit-left">
+										<span>CONTINUE</span>
+									</div>
+									<div class="submit-right">
+									<span>→</span>
+									</div>
+								</div>
+								<div class="form-side-nav">
+									<div class="nav-inner-single">
+										<span class="number">
+										01
+										</span>
+										<span class="name">
+										Personal
+										</span>
+									</div>
+									<div class="nav-inner-single active">
+										<span class="number">
+										02
+										</span>
+										<span class="name">
+										Personal
+										</span>
+									</div>
+									<div class="nav-inner-single">
+										<span class="number">
+										03
+										</span>
+										<span class="name">
+										Personal
+										</span>
+									</div>
+									
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>	
+		
+		</main>
+		<footer>
+			
+		</footer>
+	</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,152 @@
+
+$(document).ready(function(){
+
+ 	validate();
+ 	left();
+ 	container_position();
+
+ 	
+
+ 	
+
+ 	$(".form-right-inner").css({display: 'none',});
+    $(".form-left-inner").css({display: 'none',});
+
+
+	$('.bg-1').css({transform: 'scale(1.1)',});
+	$('.bg-2').css({bottom: '0px', right: '0px',});
+
+
+	$('.form-left-inner').delay(1000).show( "slide", { direction: "right" }, 500 , function(){
+		//1.5
+		$('.form-right-inner').show( "slide", { direction: "left" }, 500 ,function(){
+			//2
+			$('.form-side-nav').show();
+			if ($(window).width() < 767)
+			{
+				$('.submit-form').delay(500).fadeIn(500);//2.5
+			}
+			else
+			{
+				$('.submit-form').delay(500).show( "slide", { direction: "left" }, 500 );//2.5
+			}
+			$('.submit-right').delay(500).queue(function (next) { 
+				//3
+				$(this).css('transform', 'scale(1)');  //3 + 1
+				next();
+		  	});
+		  	$('.submit-right span').delay(2000).fadeIn(500);
+
+		});
+		$('.form-right-header h2').show( "slide", { direction: "left" }, 500 );
+		$('.added_line').each(function(){
+			$(this).show( "slide", { direction: "left" }, 500 );  
+		});
+	}); 
+
+
+	$("label.gender input").change(function(){
+		if(!$(this).hasClass('active_radio'))
+		{
+			$("label.gender").each(function(){
+				$(this).removeClass('active_radio');
+			})
+			$(this).parent('label.gender').addClass('active_radio');
+		}
+	})
+
+	function validate() {
+		$('.submit-form').on('click', function(){
+			$('#submit-hidden').click();
+		})
+	}
+	
+
+	function left()
+	{
+		
+		block_resize();
+		lines($height_inner, 18);
+		$('.form-left-inner').on('resize orientationchange', function(){
+			block_resize();
+		})
+	}
+
+	function lines($height, $line_number)
+	{
+
+		$margin = (parseInt($height / $line_number) -2) / $height * 100; //minus wysokość
+		for (var i = 0 ; i <= $line_number; i++) {
+			$left = randomInt(1,6);
+			$right = randomInt(6,9);
+			if($left>$right){
+				$left = [$right, $right = $left][0];
+			}
+			if($left==$right){
+				$right++;
+			}
+			$div =  "<div class='added_line' data-start="+$left+" data-finish="+$right+" style='top:"+$margin*i+"%'></div>";
+			$( ".form-left-inner" ).append($div);
+		}
+
+		$colors =['#531fc0', '#1cd4a3','#ffffff'];
+		$last_i = $colors.length+1;
+		$('.added_line').each(function(){
+	    	do {
+			    $current_i = parseInt(Math.floor(Math.random()*$colors.length+1));
+			}
+			while ($current_i == $last_i);
+			$last_i = $current_i;
+
+			$(this).css('background-color', $colors[$current_i-1]);
+			$left = $(this).data("start") * 10;
+			$width = $(this).data("finish") * 10  - $left ;
+
+			$(this).css('margin-left', $left +'%');
+			$(this).css('width', $width +'%');
+			
+		})
+	}
+
+	function block_resize()
+	{
+		$height_full = $('.form-right-inner').outerHeight();
+		$height_inner = $('.form-right-inner').height();
+		$padding = ($height_full - $height_inner)/2;
+		$('.form-left-inner').height($height_inner);
+	}
+
+	function container_position(){
+
+		if($("#main-form .container").height() < $(window).height())
+		{
+			$("#main-form .container").addClass('container_center');
+		}
+
+		$(window).on('load resize orientationchange', function(){
+			
+			if($("#main-form .container").height() < $(window).height())
+			{
+
+				$("#main-form .container").addClass('container_center');
+			}
+			else
+			{
+				$("#main-form .container").removeClass('container_center');
+			}
+		})
+
+ 	}
+
+	function randomInt(min,max)
+	{
+	    return Math.floor(Math.random()*(max-min+1)+min);
+	}
+
+
+
+});
+
+
+
+	
